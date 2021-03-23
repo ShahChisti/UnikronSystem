19ca4d27d57052800653da41bae006910486a257
Parth Patel <P2537693@my365.dmu.ac.uk>
2021-03-23 12:22:18 +0000

    Adding Testing

--- Unikron Testing/tstCustomer.cs
+++ Unikron Testing/tstCustomer.cs
@@ -9,11 +9,64 @@ namespace Unikron_Testing
 	{
 		[TestMethod]
 		public void InstanceOK()
-		{
+        { 
+		
             clsCustomer AnCustomer = new clsCustomer();
             ///Test if exsit
             Assert.IsNotNull(AnCustomer);
 
-		}
-	}
+		
 }
+
+        [TestMethod]
+        public void TownOK()
+{
+    clsCustomer AnCustomer = new clsCustomer();
+    String Testdata;
+    Testdata = "Leicester";
+    AnCustomer.Town = Testdata;
+    Assert.AreEqual(Testdata, AnCustomer.Town);
+
+}
+
+
+
+[TestMethod]
+public void StreetOK()
+{
+    clsCustomer AnCustomer = new clsCustomer();
+    string Testdata;
+    Testdata = "12 DMU Road";
+    AnCustomer.Street = Testdata;
+    Assert.AreEqual(Testdata, AnCustomer.Street);
+}
+
+[TestMethod]
+public void NameOK()
+{
+    clsCustomer AnCustomer = new clsCustomer();
+    string Testdata;
+    Testdata = "";
+    AnCustomer.Name = Testdata;
+    Assert.IsNotNull(Testdata, AnCustomer.Name);
+
+}
+
+
+    
+
+[TestMethod]
+public void FirstNameOK()
+{
+    clsCustomer AnCustomer = new clsCustomer();
+    string Testdata = "Parth";
+    AnCustomer.FirstName = "";
+    Assert.AreNotEqual(Testdata, AnCustomer.FirstName);
+
+
+}
+        
+
+  
+    
+
--- UnikronClasses/clsCustomer.cs
+++ UnikronClasses/clsCustomer.cs
@@ -2,5 +2,9 @@
 {
     public class clsCustomer
     {
+        public string Town { get; set; }
+        public string Street { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
     }
 }
